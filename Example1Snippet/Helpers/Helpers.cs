60d1d2c07778ac8080263e4896b3c5b4a54c5ccd
Kim G <kontakt@kimdamgroenhoej.dk>
2017-08-31 06:57:07 +0200

    Update

---
+++ Example1Snippet/Helpers/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Example1Snippet.Helpers
+{
+    public static class Helpers
+    {
+        public static HtmlString NavigationLink(this HtmlHelper html,
+            string linkText,
+            string actionName,
+            string controllerName)
+        {
+            string contextAction = (string)html.ViewContext.RouteData.Values["action"];
+            string contextController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            bool isCurrent =
+                string.Equals(contextAction, actionName, StringComparison.CurrentCultureIgnoreCase) &&
+                string.Equals(contextController, controllerName, StringComparison.CurrentCultureIgnoreCase);
+
+            return html.ActionLink(
+                linkText,
+                actionName,
+                controllerName,
+                routeValues: null,
+                htmlAttributes: isCurrent ? new { @class = "current" } : null);
+        }
+    }
+}
